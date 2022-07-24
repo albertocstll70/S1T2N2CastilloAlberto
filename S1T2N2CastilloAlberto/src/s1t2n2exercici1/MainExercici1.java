1cc729ed3496c20d5f28004ee6e22861c48fbf5b
alberto <albertocstll70@gmail.com>
2022-07-24 06:08:11 +0200

    S1T2N2CastilloAlberto/

---
+++ S1T2N2CastilloAlberto/src/s1t2n2exercici1/MainExercici1.java
@@ -0,0 +1,30 @@
+package s1t2n2exercici1;
+
+public class MainExercici1 {
+
+	public static void main(String[] args) {
+
+//		byte enteroByte = Entrada.leerByte(" Introduce un entero  de rango byte");
+//		Entrada.escribir(enteroByte);
+//
+//		int entero = Entrada.leerInt(" Introduce un entero ");
+//		Entrada.escribir(entero);
+//
+//		float numeroFloat = Entrada.leerFloat(" Introduce un numero de tipo float");
+//		Entrada.escribir(numeroFloat);
+//
+//		double numeroDouble = Entrada.leerDouble(" Introducir un número double ");
+//		Entrada.escribir(numeroDouble);
+//
+//		char letra = Entrada.leerChar(" Introduce un caracter");
+//		Entrada.escribir(letra);
+//
+//		String cadena = Entrada.leerString(" Introduce un string");
+//		Entrada.escribir(cadena);
+
+		boolean b = Entrada.leerSiNo("Introduce s o n");
+		Entrada.escribir(b);
+
+	}
+
+}
